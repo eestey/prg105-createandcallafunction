d687e54d64790f2955a838b291cb992a12d6b977
eestey <eestey@students.mchenry.edu>
2016-09-07 22:09:28 -0500

    Add files via upload

---
+++ Create and call a function.py
@@ -0,0 +1,8 @@
+def sing_angels():
+    print("Shout outs my connect though, keep a watch out for them Winslows")
+    print("I'mma get by while the world gon' turn")
+    print("I'mma get mine like you gon' get yours")
+
+sing_angels()
+sing_angels()
+sing_angels()
